02f6f59e3afa93a3e7302d1af321a30d7f6ea43e
Fernando Sonego <fsonego@algeiba.com>
2018-12-22 09:49:17 -0300

    Carga Inicial

---
+++ azure-devops-deploy-webapp-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace azure_devops_deploy_webapp_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ azure-devops-deploy-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace azure_devops_deploy_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
